83ee8d9998434d7a0ba5a2246bba0f877f18cc49
alfredlam42 <alfred.lam.42@gmail.com>
2017-01-07 15:43:15 -0800

    create list as well as individual page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,10 @@ end
 
 get '/pokemon' do
   pokemons = Array.new.concat(Pokemon.all)
-  handlebars :'/pokemon/index', locals: {pokemons: starters, }
+  handlebars :'/pokemon/index', locals: {pokemons: pokemons}
+end
+
+get '/pokemon/:pokedexNum' do
+  pokemon = Pokemon.find_by(pokedex_number: params[:pokedexNum])
+  handlebars :'/pokemon/show', locals: {pokemon: pokemon}
 end
