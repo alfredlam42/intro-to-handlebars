5b28337973235feaf86ea4d6bb8f3357c6d28fc8
alfredlam42 <alfred.lam.42@gmail.com>
2017-01-07 14:47:18 -0800

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-Pokemon.create({name: 'Bulbasaur', affinity: 'Grass/Poison'})
-Pokemon.create({name: 'Squirtle', affinity: 'Water'})
-Pokemon.create({name: 'Charmander', affinity: 'Fire'})
-Pokemon.create({name: 'Pikachu', affinity: 'Electric'})
-Pokemon.create({name: 'Eevee', affinity: 'Normal'})
+Pokemon.create(name: 'Bulbasaur', affinity: 'Grass', pokedex_number: 1)
+Pokemon.create(name: 'Squirtle', affinity: 'Water', pokedex_number: 4)
+Pokemon.create(name: 'Charmander', affinity: 'Fire', pokedex_number: 7)
